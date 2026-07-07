034d81c3b00b3d0b309aed5e11b157e7a6d95ee9
suleman12345678 <113000732+suleman12345678@users.noreply.github.com>
2026-07-07 17:37:41 +0530

    Delete revochat-ai-manual 2/revochat-manual directory

--- revochat-ai-manual 2/revochat-manual/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    qualities: [75, 90, 100],
-    deviceSizes: [640, 768, 1024, 1280, 1600, 1920, 2400],
-  },
-};
-
-export default nextConfig;
